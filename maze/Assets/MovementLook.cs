ccbae9964ab9af1363453f8304007e0a4638b945
Sacha Fanefjord <sdfa.skp@edu.mercantec.dk>
2020-06-16 12:13:32 +0200

    first

---
+++ maze/Assets/MovementLook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementLook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePos = Input.mousePosition;
+        var mousePosWorld = Camera.main.ScreenToWorldPoint(mousePos);
+
+        var timToMousePosistion = mousePosWorld - transform.position;
+        timToMousePosistion.z = 0;
+
+        transform.right = timToMousePosistion;
+
+
+    }
+}
---
+++ maze/Assets/cameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var playerPos = player.position;
+        playerPos.z = transform.position.z;
+        transform.position = playerPos;
+        
+
+    }
+}
